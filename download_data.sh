11c33bd7bdd8ad1fee80d6439890e929145fc770
Lang-Chi Yu <b99901132@ntu.edu.tw>
2017-01-10 16:51:01 +0800

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -2,5 +2,5 @@ wget http://speech.ee.ntu.edu.tw/~wjohn1483/ADL_final_QA/ADL_final_QA_dataset.zi
 unzip ADL_final_QA_dataset.zip
 mv Squad/training_data.json data/s_train.json
 mv TOEFL_QA/train.json data/t_train.json
-rm -r Squad TOEFL_QA
-rm ADL_final_QA_dataset.zip
+rm -rf Squad TOEFL_QA
+rm -f ADL_final_QA_dataset.zip
