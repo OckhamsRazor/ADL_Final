1700f4045dee84a14b3446ce5c1606e95bb71723
evelyn <c62066206@hotmail.com>
2017-01-10 08:26:37 +0800

    fix exe

--- run_squad.sh
+++ run_squad.sh
@@ -1,4 +1,4 @@
 input_file=$1
 output_file=$2
-python dmn_test.py -i $input_file
+python squad_test.py -i $input_file
 mv answer.txt $output_file
--- run_toefl.sh
+++ run_toefl.sh
@@ -1,4 +1,4 @@
 input_file=$1
 output_file=$2
-python dmn_test.py -i $input_file
+python toefl_test.py -i $input_file
 mv answer.txt $output_file
